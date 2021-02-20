c10d65a1635ff5145a959d757df9be3575ee361f
SerBABA <dan2ronen@gmail.com>
2021-02-21 02:16:09 +1300

    Obstacles are now implemented. Only source and target visual implementation remaining before visual overhall.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,100 +1,205 @@
 import React, { useState } from "react";
-import { NodeClass, getAStarSteps, GridClass } from "./a_star";
+import { NodeClass, getAStarSteps, GridClass, nodeState } from "./a_star";
 import "./App.css";
 
+const A_STAR_UPDATE_MS_DELAY = 45;
+const X_SIZE = 8;
+const Y_SIZE = 8;
+
+enum changeEnum {
+  target = "target",
+  source = "source",
+  obstacle = "obstacle",
+  none = "none",
+}
+
 function App() {
-    // base grid of the entire background.
-    const grid = new GridClass(5, 5);
-    const [running, setRunning] = useState(false);
+  // base grid of the entire background.
+  const [grid, setGrid] = useState(new GridClass(X_SIZE, Y_SIZE));
+  const [running, setRunning] = useState(false);
 
-    // change this to modify based on where it is clicked on screen!
-    const sourceNode = grid.getNode(1, 1);
-    const targetNode = grid.getNode(4, 3);
+  // Keeps the state to show of the grid
+  const [gridState, setGridState] = useState(grid.state);
 
-    // Keeps the state to show of the grid
-    const [gridState, setGridState] = useState(grid.state);
+  // Stores the actual coordiantes. unable to force the type sadly :(
+  const [sourceCoords, setSourceCoords] = useState({ x: 2, y: 3 });
+  const [targetCoords, setTargetCoords] = useState({ x: 7, y: 2 });
 
-    // This runs the software to create the history states.
-    const handleRunClick = (sourceNode: NodeClass, targetNode: NodeClass) => {
-        getAStarSteps(grid, sourceNode, targetNode)
-            .then((stateHistory) => {
-                setRunning(true);
-                for (let i = 0; i < stateHistory.length; i++) {
-                    setTimeout(() => {
-                        setGridState(stateHistory[i]);
-                        if (i + 1 >= stateHistory.length) setRunning(false);
-                    }, 100 * i);
-                }
-            })
-            .catch(() => {});
-    };
+  // Stores the source and target nodes
+  const sourceNode = grid.getNode(sourceCoords.x, sourceCoords.y);
+  const targetNode = grid.getNode(targetCoords.x, targetCoords.y);
 
-    // Resets the grid to its original state.
-    const handleResetClick = async () => {
-        const originalGridState = await grid.resetGridState();
-        setGridState(originalGridState);
-    };
+  // Similar to a radio button
+  const [selectionType, setSelectionType] = useState(changeEnum.none);
 
-    // We beed the source and target nodes to exist to run the A* algorithm
-    if (sourceNode && targetNode) {
-        return (
-            <div>
-                <Grid grid={gridState} xSize={grid.xSize} ySize={grid.ySize}></Grid>
-                <button onClick={() => handleRunClick(sourceNode, targetNode)} disabled={running}>
-                    run!
-                </button>
-                <button onClick={handleResetClick} disabled={running}>
-                    reset!
-                </button>
-            </div>
-        );
-    } else {
-        return <span>Missing source and target node!</span>;
+  // Accessing grid?
+  const getGrid = (): GridClass => {
+    return grid;
+  };
+
+  // Handles the change of the target node.
+  const handleChangeEnumAssignment = (x: number, y: number) => {
+    if (running) return;
+
+    switch (selectionType) {
+      case changeEnum.target:
+        setTargetCoords({ x, y });
+        break;
+
+      case changeEnum.source:
+        setSourceCoords({ x, y });
+        break;
+
+      case changeEnum.obstacle:
+        let node = grid.getNode(x, y);
+        let newValue = nodeState.obstacle;
+        if (node) {
+          // the new value
+          if (node.state === nodeState.obstacle) {
+            newValue = nodeState.unknown;
+          }
+
+          // Update the visual
+          var gridStateCopy = gridState.slice();
+          gridStateCopy[getGrid().getIndex(x, y)].state = newValue;
+          setGridState(gridStateCopy);
+
+          // Update the grid state ++ add safety for non real nodes
+          node.state = newValue;
+          setGrid(grid);
+        }
+        break;
+
+      default:
+        alert("Select an assignment please!");
     }
+  };
+
+  // This runs the software to create the history states.
+  const handleRunClick = (sourceNode: NodeClass, targetNode: NodeClass) => {
+    getAStarSteps(grid, sourceNode, targetNode)
+      .then((stateHistory) => {
+        setRunning(true);
+        for (let i = 0; i < stateHistory.length; i++) {
+          setTimeout(() => {
+            setGridState(stateHistory[i]);
+            if (i + 1 >= stateHistory.length) setRunning(false);
+          }, A_STAR_UPDATE_MS_DELAY * i);
+        }
+      })
+      .catch(() => {});
+  };
+
+  // Resets the grid to its original state.
+  const handleResetClick = async () => {
+    const originalGridState = await grid.resetGridState();
+    setGridState(originalGridState);
+  };
+
+  // We beed the source and target nodes to exist to run the A* algorithm
+  if (sourceNode && targetNode) {
+    return (
+      <div>
+        <Grid
+          grid={gridState}
+          xSize={grid.xSize}
+          ySize={grid.ySize}
+          handleChange={handleChangeEnumAssignment}
+        ></Grid>
+        <button onClick={() => handleRunClick(sourceNode, targetNode)} disabled={running}>
+          run!
+        </button>
+        <button onClick={handleResetClick} disabled={running}>
+          reset!
+        </button>
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="source"
+              name="action"
+              onClick={() => setSelectionType(changeEnum.source)}
+              disabled={running}
+            ></input>
+            Source
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="target"
+              name="action"
+              onClick={() => setSelectionType(changeEnum.target)}
+              disabled={running}
+            ></input>
+            Target
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="obstacle"
+              name="action"
+              onClick={() => setSelectionType(changeEnum.obstacle)}
+              disabled={running}
+            ></input>{" "}
+            Obstacle
+          </label>
+        </div>
+      </div>
+    );
+  } else {
+    return <span>Missing source and target node!</span>;
+  }
 }
 
 // Grid represents and contains the grid appearence.
 type gridProps = {
-    grid: NodeClass[];
-    xSize: number;
-    ySize: number;
+  grid: NodeClass[];
+  xSize: number;
+  ySize: number;
+  handleChange: (x: number, y: number) => void;
 };
 function Grid(props: gridProps) {
-    return (
-        <div
-            className="grid"
-            style={{
-                gridTemplateColumns: `repeat(${props.xSize}, 1fr)`,
-                gridTemplateRows: `repeat(${props.ySize}, 1fr)`,
-            }}
-        >
-            {props.grid.map((node, index) => {
-                return <Slot data={node} key={index} item={index} />;
-            })}
-        </div>
-    );
+  return (
+    <div
+      className="grid"
+      style={{
+        gridTemplateColumns: `repeat(${props.xSize}, 1fr)`,
+        gridTemplateRows: `repeat(${props.ySize}, 1fr)`,
+      }}
+    >
+      {props.grid.map((node, index) => {
+        return <Slot data={node} key={index} item={index} handleChange={props.handleChange} />;
+      })}
+    </div>
+  );
 }
 
 // Slot represents a node on a grid.
 type slotProps = {
-    data: NodeClass;
-    item: number;
+  data: NodeClass;
+  item: number;
+  handleChange: (x: number, y: number) => void;
 };
 function Slot(props: slotProps) {
-    const data = props.data;
+  const data = props.data;
 
-    return (
-        <div className={`square-slot state-${data.state}`}>
-            <h6>
-                {data.x}, {data.y}, index: ({props.item})
-            </h6>
-            <p>{data.knownDistance}</p>
-            <p>{data.state}</p>
-            <p>
-                parent({data.parent?.x}, {data.parent?.y})
-            </p>
-        </div>
-    );
+  return (
+    <div
+      className={`square-slot state-${data.state}`}
+      onClick={() => props.handleChange(data.x, data.y)}
+    >
+      <h6>
+        {data.x}, {data.y}, index: ({props.item})
+      </h6>
+      <p>
+        {data.knownDistance} + {data.endDistance || "Unknown"}
+      </p>
+      <p>{data.state}</p>
+      <p>
+        parent({data.parent?.x}, {data.parent?.y})
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- app/src/a_star.ts
+++ app/src/a_star.ts
@@ -6,261 +6,262 @@ const Lodash = require("lodash");
 
 // Differnt states the node can be in.
 export enum nodeState {
-    processed = "processed",
-    unknown = "unknown",
-    seen = "seen",
-    obstacle = "obstacle",
-    path = "path",
+  processed = "processed",
+  unknown = "unknown",
+  seen = "seen",
+  obstacle = "obstacle",
+  path = "path",
 }
 
 // coordinates type
-type coords = {
-    x: number;
-    y: number;
+export type Coords = {
+  x: number;
+  y: number;
 };
 
 // Used to store data about each node.
 export class NodeClass {
-    x: number;
-    y: number;
-    state: nodeState;
-    knownDistance: number;
-    endDistance: number | null;
-    parent: NodeClass | null;
+  x: number;
+  y: number;
+  state: nodeState;
+  knownDistance: number;
+  endDistance: number | null;
+  parent: NodeClass | null;
 
-    constructor(x: number, y: number) {
-        // Position on grid
-        this.x = x;
-        this.y = y;
+  constructor(x: number, y: number) {
+    // Position on grid
+    this.x = x;
+    this.y = y;
 
-        // Defaults for tracking
-        this.state = nodeState.unknown;
-        this.knownDistance = Number.POSITIVE_INFINITY;
-        this.endDistance = null;
-        this.parent = null;
-    }
+    // Defaults for tracking
+    this.state = nodeState.unknown;
+    this.knownDistance = Number.POSITIVE_INFINITY;
+    this.endDistance = null;
+    this.parent = null;
+  }
 }
 
 // Stores the data about a grid.
 export class GridClass {
-    xSize: number;
-    ySize: number;
-    state: NodeClass[];
+  xSize: number;
+  ySize: number;
+  state: NodeClass[];
 
-    constructor(xSize: number, ySize: number) {
-        this.xSize = xSize;
-        this.ySize = ySize;
+  constructor(xSize: number, ySize: number) {
+    this.xSize = xSize;
+    this.ySize = ySize;
 
-        this.state = this.createDefaultState();
+    this.state = this.createDefaultState();
+  }
+
+  // Create and populates a default state to return. This contains all the nodes.
+  createDefaultState(): NodeClass[] {
+    let state = [];
+
+    for (let y = 0; y < this.ySize; y++) {
+      for (let x = 0; x < this.xSize; x++) {
+        state.push(new NodeClass(x, y));
+      }
     }
 
-    // Create and populates a default state to return. This contains all the nodes.
-    createDefaultState(): NodeClass[] {
-        let state = [];
+    return state;
+  }
 
-        for (let y = 0; y < this.ySize; y++) {
-            for (let x = 0; x < this.xSize; x++) {
-                state.push(new NodeClass(x, y));
-            }
-        }
-
-        return state;
+  // Resetting the grid elements to unknown when they are not unknown or an obstacle.
+  // Returns this original state of the grid
+  resetGridState(): NodeClass[] {
+    for (let i = 0; i < this.state.length; i++) {
+      if (this.state[i].state !== nodeState.unknown && this.state[i].state !== nodeState.obstacle)
+        this.state[i].state = nodeState.unknown;
+      this.state[i].parent = null;
+      this.state[i].knownDistance = Number.POSITIVE_INFINITY;
+      this.state[i].endDistance = null;
     }
+    return this.state;
+  }
 
-    // Resetting the grid elements to unknown when they are not unknown or an obstacle.
-    // Returns this original state of the grid
-    resetGridState(): NodeClass[] {
-        for (let i = 0; i < this.state.length; i++) {
-            if (
-                this.state[i].state !== nodeState.unknown ||
-                this.state[i].state !== nodeState.obstacle
-            )
-                this.state[i].state = nodeState.unknown;
-        }
-        return this.state;
+  // Returns the index of a node at given coordiantes
+  getIndex(x: number, y: number): number {
+    return x + y * this.xSize;
+  }
+
+  // Get the Node that resides at the x, y coordinates
+  getNode(x: number, y: number): NodeClass | null {
+    if (this.isInBounds(x, y)) {
+      return this.state[this.getIndex(x, y)];
+    } else {
+      return null;
     }
+  }
 
-    // Get the Node that resides at the x, y coordinates
-    getNode(x: number, y: number): NodeClass | null {
-        if (this.isInBounds(x, y)) {
-            return this.state[x + y * this.xSize];
-        } else {
-            return null;
-        }
+  // Returns the distance between two nodes on the grid.
+  distanceBetween(node: NodeClass, otherNode: NodeClass) {
+    let xDiff = Math.abs(node.x - otherNode.x);
+    let yDiff = Math.abs(node.y - otherNode.y);
+
+    if (xDiff === 0 || yDiff === 0) {
+      return Math.max(xDiff, yDiff) ** 2;
+    } else {
+      return xDiff ** 2 + yDiff ** 2;
     }
+  }
 
-    // Returns the distance between two nodes on the grid.
-    distanceBetween(node: NodeClass, otherNode: NodeClass) {
-        let xDiff = Math.abs(node.x - otherNode.x);
-        let yDiff = Math.abs(node.y - otherNode.y);
-
-        if (xDiff === 0 || yDiff === 0) {
-            return Math.max(xDiff, yDiff) ** 2;
-        } else {
-            return xDiff ** 2 + yDiff ** 2;
-        }
-    }
-
-    // Given x, y coordinates return wether or not the node would be in bounds of the
-    // state grid.
-    isInBounds(x: number, y: number): boolean {
-        return 0 <= x && x < this.xSize && 0 <= y && y < this.ySize;
-    }
+  // Given x, y coordinates return wether or not the node would be in bounds of the
+  // state grid.
+  isInBounds(x: number, y: number): boolean {
+    return 0 <= x && x < this.xSize && 0 <= y && y < this.ySize;
+  }
 }
 
 // Gets the minimum node
 function getMinimumNode(nodes: NodeClass[]) {
-    let minNode: NodeClass | null = null;
-    let minDistance: number = Number.POSITIVE_INFINITY;
+  let minNode: NodeClass | null = null;
+  let minDistance: number = Number.POSITIVE_INFINITY;
 
-    for (let i = 0; i < nodes.length; i++) {
-        let node = nodes[i];
+  for (let i = 0; i < nodes.length; i++) {
+    let node = nodes[i];
 
-        if (node.endDistance) {
-            if (
-                node.knownDistance + node.endDistance < minDistance &&
-                node.state === nodeState.seen
-            ) {
-                minNode = node;
-                minDistance = node.knownDistance + node.endDistance;
-            }
-        } else {
-            if (nodes[i].knownDistance < minDistance && node.state === nodeState.seen) {
-                minNode = node;
-                minDistance = node.knownDistance;
-            }
-        }
+    if (node.endDistance) {
+      if (node.knownDistance + node.endDistance < minDistance && node.state === nodeState.seen) {
+        minNode = node;
+        minDistance = node.knownDistance + node.endDistance;
+      }
+    } else {
+      if (nodes[i].knownDistance < minDistance && node.state === nodeState.seen) {
+        minNode = node;
+        minDistance = node.knownDistance;
+      }
     }
+  }
 
-    return minNode;
+  return minNode;
 }
 
 // iterates over the neighbours of the current node and returns them (within bounds of the grid).
 function getNeighbours(grid: GridClass, currNode: NodeClass) {
-    let neighbours: NodeClass[] = [];
+  let neighbours: NodeClass[] = [];
 
-    // neighbour coords depending on the current node coordinates.
-    const neighboursCoords: coords[] = [
-        { x: currNode.x + 1, y: currNode.y + 1 },
-        { x: currNode.x, y: currNode.y + 1 },
-        { x: currNode.x - 1, y: currNode.y + 1 },
-        //
-        { x: currNode.x + 1, y: currNode.y },
-        { x: currNode.x - 1, y: currNode.y },
-        //
-        { x: currNode.x + 1, y: currNode.y - 1 },
-        { x: currNode.x, y: currNode.y - 1 },
-        { x: currNode.x - 1, y: currNode.y - 1 },
-    ];
+  // neighbour coords depending on the current node coordinates.
+  const neighboursCoords: Coords[] = [
+    { x: currNode.x + 1, y: currNode.y + 1 },
+    { x: currNode.x, y: currNode.y + 1 },
+    { x: currNode.x - 1, y: currNode.y + 1 },
+    //
+    { x: currNode.x + 1, y: currNode.y },
+    { x: currNode.x - 1, y: currNode.y },
+    //
+    { x: currNode.x + 1, y: currNode.y - 1 },
+    { x: currNode.x, y: currNode.y - 1 },
+    { x: currNode.x - 1, y: currNode.y - 1 },
+  ];
 
-    for (let i = 0; i < neighboursCoords.length; i++) {
-        let coord = neighboursCoords[i];
-        let node = grid.getNode(coord.x, coord.y);
+  for (let i = 0; i < neighboursCoords.length; i++) {
+    let coord = neighboursCoords[i];
+    let node = grid.getNode(coord.x, coord.y);
 
-        // if the node is returned it is in bounds
-        if (node) {
-            // check that they are not an obstacle or processed.
-            if (node.state !== nodeState.obstacle && node.state !== nodeState.processed)
-                neighbours.push(node);
-        }
+    // if the node is returned it is in bounds
+    if (node) {
+      // check that they are not an obstacle or processed.
+      if (node.state !== nodeState.obstacle && node.state !== nodeState.processed)
+        neighbours.push(node);
     }
+  }
 
-    return neighbours;
+  return neighbours;
 }
 
 // Adds a new entry into the state history of the A* steps
 function addNewStateEntry(stateHistory: NodeClass[][], newState: NodeClass[]) {
-    stateHistory.push(Lodash.cloneDeep(newState));
+  stateHistory.push(Lodash.cloneDeep(newState));
 }
 
 // Generates the steps to present to generate the A* algorithm steps
 export async function getAStarSteps(
-    grid: GridClass,
-    source: NodeClass,
-    target: NodeClass
+  grid: GridClass,
+  source: NodeClass,
+  target: NodeClass
 ): Promise<NodeClass[][]> {
-    // ensure we have a consistent start state if it is re-run
-    await grid.resetGridState();
+  // ensure we have a consistent start state if it is re-run
+  await grid.resetGridState();
 
-    // initialize the source node.
-    let sourceNode = grid.getNode(source.x, source.y);
-    if (sourceNode) {
-        sourceNode.state = nodeState.seen;
-        sourceNode.knownDistance = 0;
-    } else {
-        throw new AssertionError({ message: "Missing start Node" });
-    }
+  // initialize the source node.
+  let sourceNode = grid.getNode(source.x, source.y);
+  if (sourceNode) {
+    sourceNode.state = nodeState.seen;
+    sourceNode.knownDistance = 0;
+  } else {
+    throw new AssertionError({ message: "Missing start Node" });
+  }
 
-    // Create a empty steps list (list of grid states).
-    let stateHistory: NodeClass[][] = [];
+  // Create a empty steps list (list of grid states).
+  let stateHistory: NodeClass[][] = [];
+  addNewStateEntry(stateHistory, grid.state);
+
+  let minNode = getMinimumNode(grid.state);
+  // start main loop
+  while (minNode) {
+    // mark min node as processed + add a step to the steps
+    minNode.state = await nodeState.processed;
     addNewStateEntry(stateHistory, grid.state);
 
-    let minNode = getMinimumNode(grid.state);
-    // start main loop
-    while (minNode) {
-        // mark min node as processed + add a step to the steps
-        minNode.state = await nodeState.processed;
-        addNewStateEntry(stateHistory, grid.state);
+    // check all neighbours for shorter paths for them.
+    let neighbours = await getNeighbours(grid, minNode);
 
-        // check all neighbours for shorter paths for them.
-        let neighbours = await getNeighbours(grid, minNode);
-        console.log({ node: minNode, neighbours: neighbours });
+    // Update their distance based on knwon_distance_from_start + distance_from_end (A*).
+    // + add step to the steps for each neighbour changed / visited.
+    for (let i = 0; i < neighbours.length; i++) {
+      let currNeighbour = neighbours[i];
 
-        // Update their distance based on knwon_distance_from_start + distance_from_end (A*).
-        // + add step to the steps for each neighbour changed / visited.
-        for (let i = 0; i < neighbours.length; i++) {
-            let currNeighbour = neighbours[i];
+      // change node state to seen if unknown.
+      if (currNeighbour.state === nodeState.unknown) currNeighbour.state = nodeState.seen;
 
-            // change node state to seen if unknown.
-            if (currNeighbour.state === nodeState.unknown) currNeighbour.state = nodeState.seen;
+      // The distance between the minimum node and the current neighbour
+      let neighbourDistance = grid.distanceBetween(minNode, currNeighbour);
 
-            // The distance between the minimum node and the current neighbour
-            let neighbourDistance = grid.distanceBetween(minNode, currNeighbour);
+      // ensures there is a end distance already calculated
+      if (!currNeighbour.endDistance)
+        currNeighbour.endDistance = grid.distanceBetween(target, currNeighbour);
 
-            // ensures there is a end distance already calculated
-            if (!currNeighbour.endDistance)
-                currNeighbour.endDistance = grid.distanceBetween(target, currNeighbour);
-
-            // This is the check for the distance
-            if (
-                currNeighbour.knownDistance >
-                minNode.knownDistance + neighbourDistance + currNeighbour.endDistance
-            ) {
-                // update current neighbour.
-                currNeighbour.knownDistance = minNode.knownDistance + neighbourDistance;
-                currNeighbour.parent = minNode;
-                // testing
-            }
-            // add step to history.
-            await addNewStateEntry(stateHistory, grid.state);
-        }
-
-        // Get new minimum node
-        if (minNode.x !== target.x || minNode.y !== target.y) {
-            minNode = await getMinimumNode(grid.state);
-        } else {
-            break;
-        }
+      // This is the check for the distance
+      if (
+        currNeighbour.knownDistance >
+        minNode.knownDistance + neighbourDistance + currNeighbour.endDistance
+      ) {
+        // update current neighbour.
+        currNeighbour.knownDistance = minNode.knownDistance + neighbourDistance;
+        currNeighbour.parent = minNode;
+        // testing
+      }
+      // add step to history.
+      await addNewStateEntry(stateHistory, grid.state);
     }
 
-    // generate the final path + step for each path node.
-    if (minNode) {
-        let currNode: NodeClass | null = target;
-        let nextNode: NodeClass | null = target.parent;
-
-        while (nextNode !== currNode) {
-            // trace back... and add states to the history!
-            if (currNode) {
-                currNode.state = nodeState.path;
-            }
-
-            currNode = nextNode;
-            if (nextNode) {
-                nextNode = nextNode.parent;
-            }
-            addNewStateEntry(stateHistory, grid.state);
-        }
+    // Get new minimum node
+    if (minNode.x !== target.x || minNode.y !== target.y) {
+      minNode = await getMinimumNode(grid.state);
+    } else {
+      break;
     }
+  }
 
-    return stateHistory;
+  // generate the final path + step for each path node.
+  if (minNode) {
+    let currNode: NodeClass | null = target;
+    let nextNode: NodeClass | null = target.parent;
+
+    while (nextNode !== currNode) {
+      // trace back... and add states to the history!
+      if (currNode) {
+        currNode.state = nodeState.path;
+      }
+
+      currNode = nextNode;
+      if (nextNode) {
+        nextNode = nextNode.parent;
+      }
+      addNewStateEntry(stateHistory, grid.state);
+    }
+  }
+
+  return stateHistory;
 }
